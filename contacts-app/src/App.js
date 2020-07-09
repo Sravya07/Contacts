7b802a49d8629efbfdc21d12c6dcc363fbf87926
Sravya Somisetty <sravya.somisetty@sjsu.edu>
2020-07-09 10:06:29 -0700

    Update App.js

--- contacts-app/src/App.js
+++ contacts-app/src/App.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "@karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "@richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "@tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
+
+class App extends Component {
+  state = {
+    contacts : [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "@karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "@richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "@tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
   }
-];
-
-const App = () => {
-  return (
-    <div>
-      <ListContacts contacts={contacts} />
-    </div>
-  );
-};
-
+  render() {
+    return (
+      <div>
+        <ListContacts contacts={this.state.contacts} />
+      </div>
+    );
+  }
+}
 
 export default App;
